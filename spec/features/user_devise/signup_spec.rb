0e7cdfff979c2b0fa969b4a8ebd8b799e08156f7
Sahil Grover <gr.sahil20@gmail.com>
2021-01-17 21:16:04 +0100

    failed signup

--- spec/features/user_devise/signup_spec.rb
+++ spec/features/user_devise/signup_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature :signup_user do
 
-	scenario "A user creates a new tweet" do
+	scenario "A user creates signup with valid credentials" do
 		User.destroy_all
 
 		visit "/users/sign_up"
@@ -19,13 +19,21 @@ RSpec.feature :signup_user do
 		expect(User.count).to eq(1)
 	end
 
-	# scenario "A user fails to create a new tweet" do
-	# 	visit "/"
-	# 	click_link "New Tweet"
-	# 	fill_in "tweet_content", with: ""
-	# 	click_button "Create Tweet"
-	# 	expect(page).to have_content(TweetsController::RECORD_NOT_CREATED)
-	# 	expect(page).to have_content("Content can't be blank")
-	# end
+	scenario "A user fails to signup with invalid credentials" do
+		User.destroy_all
+
+		visit "/users/sign_up"
+		password = Faker::Internet.password(min_length: 8)
+		fill_in "user_email", with: ""
+		fill_in "user_username", with: Faker::Internet.username(separators: %w())
+		fill_in "user_full_name", with: Faker::Name.name
+		fill_in "user_password", with: password
+		fill_in "user_password_confirmation", with: password
+		click_button "Sign Up"
+
+		expect(page).to have_content("Please review the problems below:")
+		expect(page.current_path).to eq('/users')
+		expect(User.count).to eq(0)
+	end
 
 end
