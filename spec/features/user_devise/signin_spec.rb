31802b9814a367d44e980f870e267b52c865b35a
Sahil Grover <gr.sahil20@gmail.com>
2021-02-03 09:15:49 +0100

    user sign out

--- spec/features/user_devise/signin_spec.rb
+++ spec/features/user_devise/signin_spec.rb
@@ -29,6 +29,14 @@ RSpec.feature :signin_user do
       expect(page).to have_content(@user.username)
       expect(page.current_path).to eq("/")
     end
+    scenario "with devise helper" do
+      sign_in(@user)
+      visit("/")
+
+      expect(page).to have_content(@user.full_name)
+      expect(page).to have_content(@user.email)
+      expect(page).to have_content(@user.username)
+    end
   end
 
   context "A user with invalid credentials not able to logs in" do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+require "spec_helper"
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-require 'shoulda/matchers'
-
+require "rspec/rails"
+require "shoulda/matchers"
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -40,6 +39,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -71,4 +71,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
